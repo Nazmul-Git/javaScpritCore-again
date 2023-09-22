3912e9fca019ce97b017c9ce2cb5253b887c9474
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-22 23:46:57 +0600

    Use a template literal to include the BMI values in the outputs.

--- operator.js
+++ operator.js
@@ -4,4 +4,14 @@ const johnMass=92;
 const johnHeight=1.95;
 
 const markHigherBMI= (marksMass/(marksHeight**2))>(johnMass/(johnHeight*johnHeight));
-console.log(markHigherBMI); // true
+// console.log(markHigherBMI); // true
+
+console.log("Mark's BMI is higher than John's! ???");
+const marksBMI=marksMass/(marksHeight**2);
+const johnBMI=johnMass/(johnHeight*johnHeight)
+if(marksBMI> johnBMI){
+    console.log(`marks bmi ${marksBMI} is higher than john ${johnBMI} !`);
+}
+else{
+    console.log(`John's BMI ${johnBMI} is higher than Mark's ${marksBMI}!`)
+}
