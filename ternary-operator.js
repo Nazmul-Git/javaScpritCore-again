f5e6a67368c29741663828fa6a4a68a9a0d237a1
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-24 00:14:44 +0600

    Ternary operator.

---
+++ ternary-operator.js
@@ -0,0 +1,14 @@
+const age =24;
+let aim;
+if(age>=7){
+    aim='Namaj aday koro.';
+}else{
+    aim='Tumi obuj sisu.';
+}
+console.log(aim); // Namaj aday koro.
+
+// in ternary operator
+
+
+const yourAim= age >=7? 'Namaj aday koro.':'Tumi obuj sisu.';
+console.log(yourAim); // Namaj aday koro.
