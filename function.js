a549bf9b669cf82010ac86efd5ebb56ca723ba98
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-26 00:26:41 +0600

    Function declaration & function expression.

--- function.js
+++ function.js
@@ -1,4 +1,25 @@
-const nameFunction=()=>{
-    console.log('I am name function.');
+//  Function Declaration
+// we can call function before declaration
+// its call hoisting.
+const age1= calAge(1998);
+
+function calAge(birthYear){
+    return 2023-birthYear;
 }
-nameFunction();
+// const age1= calAge(1998);
+console.log(age1);
+
+// ------------------------------------------------------------
+
+//  Function Expression
+// Cannot access 'calAge2' before initialization.
+//  Thats the reason function expression is best.
+
+// const age2=calAge2(1997);
+
+
+const calAge2= function(birthYear){
+    return 2023-birthYear;
+}
+const age2=calAge2(1997);
+console.log(age2);
