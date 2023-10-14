5d902ddf402cb79f0efcd64e31650efbaf8ed34d
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-10-15 01:35:03 +0600

    when we declare var firstName then output: sunny which come from browser--> window--> firstName.

--- this.js
+++ this.js
@@ -1,4 +1,7 @@
 
+var firstName='sunny'; // when we declared a global variable var,
+//for arrow function, var create a global variable on browser window. 
+
 const jonas={
     firstName:'jonas',
     year:1995,
@@ -7,13 +10,13 @@ const jonas={
     },
     calName: ()=>{
         console.log(this); //In arrow function this keyword target on browser window. 
-        console.log(this.firstName);
+        console.log(this.firstName); //1st: undefined //2nd when dec var firstName: sunny
     }
     
 }
 
 jonas.calAge(); //output: 28
-jonas.calName(); //undefined & target browser window
+jonas.calName(); //undefined & this target browser window  //when we declare var firstName then output: sunny
 
 
 
