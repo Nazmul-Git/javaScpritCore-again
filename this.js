014f6c2a7fd7af168cfabd852eb448817f91f553
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-10-15 02:09:56 +0600

    In function expression this keyword target the browser window and give the output undefined.

--- this.js
+++ this.js
@@ -1,22 +1,34 @@
 
-var firstName='sunny'; // when we declared a global variable var,
+// var firstName='sunny'; // when we declared a global variable var,
 //for arrow function, var create a global variable on browser window. 
 
 const jonas={
     firstName:'jonas',
-    year:1995,
+    year:1960,
     calAge: function(){
         console.log(2023-this.year);
+
+        // this in function expression
+        
+        const calAgeExpr=function(){
+            console.log(this);  // target the browser window
+            console.log(this.year); //undefined
+            
+        };
+        calAgeExpr();
     },
     calName: ()=>{
-        console.log(this); //In arrow function this keyword target on browser window. 
-        console.log(this.firstName); //1st: undefined //2nd when dec var firstName: sunny
-    }
+        // console.log(this); //In arrow function this keyword target on browser window. 
+        // console.log(this.firstName); //1st: undefined //2nd when dec var firstName: sunny
+    },
     
-}
+    
+};
 
-jonas.calAge(); //output: 28
-jonas.calName(); //undefined & this target browser window  //when we declare var firstName then output: sunny
+// jonas.calAge(); //output: 28
+jonas.calName(); //undefined & this target browser window  //when we declare var firstName then output: sunny which come from browser--> window--> firstName
+
+jonas.calAge();
 
 
 
