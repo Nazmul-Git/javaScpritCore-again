2864e435a7d8136127a2d448481438230da673c3
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-10-16 01:57:41 +0600

    Primitive type stored in CALL STACK & reference type are stored in HEAP.

--- primitive-Vs-ReferenceType.js
+++ primitive-Vs-ReferenceType.js
@@ -10,5 +10,16 @@ console.log(oldAge); // 30 //but oldAge =30   why????????
 // for reference type
 const me={
     name:'sunny',
-    
-}
+    age:25
+};
+const islam=me;
+islam.age=1; // age value is the same this 2 object...
+console.log('Islam: ',islam); //Islam:  { name: 'sunny', age: 1 }
+console.log('Me :',me);  //Me : { name: 'sunny', age: 1 }
+
+
+
+/*
+What happening here ??
+
+*/
