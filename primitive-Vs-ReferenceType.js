ddf1d85350cbd4e0da430440d08fdfa2ced8b3cb
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-10-16 03:43:34 +0600

    It's all about of js behind the seen.

--- primitive-Vs-ReferenceType.js
+++ primitive-Vs-ReferenceType.js
@@ -3,8 +3,8 @@ let age=30;
 let oldAge=age; //32 oldAge a assign haor kotha...!!
 
 age=32;
-console.log(age); // 32
-console.log(oldAge); // 30 //but oldAge =30   why????????
+// console.log(age); // 32
+// console.log(oldAge); // 30 //but oldAge =30   why????????
 
 
 //2. for reference type
@@ -14,8 +14,8 @@ const me={
 };
 const islam=me;
 islam.age=1; // age value is the same this 2 object...
-console.log('Islam: ',islam); //Islam:  { name: 'sunny', age: 1 }
-console.log('Me :',me);  //Me : { name: 'sunny', age: 1 }
+// console.log('Islam: ',islam); //Islam:  { name: 'sunny', age: 1 }
+// console.log('Me :',me);  //Me : { name: 'sunny', age: 1 }
 
 
 
@@ -26,12 +26,24 @@ Details in notebook.
 
 
 
-// 3. copying object
+// 3. copying one new object to other
 const me2={
     name:'sunny',
-    age:25
+    age:25,
+    language:['c','js']
 };
-console.log(me2);   //{ name: 'sunny', age: 25 }
-const meCopy=Object.assign({}, me2);
-meCopy.age=1;
-console.log(meCopy); //{ name: 'sunny', age: 1 }
+// console.log(me2);   //{ name: 'sunny', age: 25 }
+const me2Copy=Object.assign({}, me2);
+me2Copy.age=1;
+// console.log(me2Copy); //{ name: 'sunny', age: 1 }
+//Copying object but still in same memory reference.Its call shallow copy .Let's see-----
+me2Copy.language.push('react.js');
+me2Copy.language.push('node.js');
+console.log(me2);  // [ 'c', 'js', 'react.js', 'node.js' ]
+console.log(me2Copy); // [ 'c', 'js', 'react.js', 'node.js' ]
+// so, still in same memory reference.
+
+
+
+// -> Deep clone [ coming soon .....]
+
